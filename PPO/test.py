9a7b3e552fa3c55077f27998613c5e5f22c7373a
Elaphurus <105095085+LennoxLiu@users.noreply.github.com>
2023-08-22 21:22:47 +0200

    A Run

--- PPO/test.py
+++ PPO/test.py
@@ -4,10 +4,10 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
 # Parallel environments
-vec_env = make_vec_env("CartPole-v1", n_envs=4,seed=42)
+vec_env = make_vec_env("CartPole-v1", n_envs=4, seed=42)
 
 model = PPO("MlpPolicy", vec_env, verbose=1)
-model.learn(total_timesteps=250)
+model.learn(total_timesteps=250000)
 model.save("ppo_cartpole")
 
 del model # remove to demonstrate saving and loading
