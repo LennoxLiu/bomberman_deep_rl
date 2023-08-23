0235b312ba008cc483f0794fe5b8d0b77ddfc4f6
Elaphurus <105095085+LennoxLiu@users.noreply.github.com>
2023-08-23 20:33:14 +0200

    Passing game_state

---
+++ PPO/CustomEnv.py
@@ -0,0 +1,59 @@
+import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
+from stable_baselines3.common.env_checker import check_env
+from gymnasium.spaces import Box, Dict, Discrete, MultiBinary, MultiDiscrete
+import settings as s
+
+
+class CustomEnv(gym.Env):
+    """Custom Environment that follows gym interface."""
+
+    # metadata = {"render_modes": ["human"], "render_fps": 30}
+
+    def __init__(self, arg1, arg2,):
+        super().__init__()
+        # Define action and observation space
+        # They must be gym.spaces objects
+        # Example when using discrete actions:
+        self.action_space = spaces.Discrete(6) # UP, DOWN, LEFT, RIGHT, WAIT, BOMB
+        
+        # Do not pass "round" and "user_input"
+        COIN_COUNT = 50
+        self.observation_space = spaces.Dict(
+            {   "step": Discrete(s.MAX_STEPS), \ 
+                "field": MultiDiscrete([s.COLS, s.ROWS, 3]), \
+                "bombs": Dict({"position": MultiDiscrete([s.COLS, s.ROWS]), "countdown": Discrete(s.BOMB_TIMER + 1)}), \
+                "explosion_map": MultiDiscrete([s.COLS, s.ROWS, s.EXPLOSION_TIMER + 1]), \
+                "coins": MultiDiscrete([s.COLS, s.ROWS]), \
+                "self": Dict({  "score": Box(low=0, dtype=np.float64), \
+                                "bomb_possible": Discrete(2), \
+                                "position": MultiDiscrete([s.COLS, s.ROWS])
+                            }), \
+                "others": Dict({    "id": Discrete(3),\
+                                    "status": Dict({    "score": Box(low=0, dtype=np.float64),\
+                                                        "bomb_possible": Discrete(2),\
+                                                        "position": MultiDiscrete([s.COLS, s.ROWS])\
+                                                    })
+                                }
+                            )
+            } 
+            )
+
+    def step(self, action):
+        
+        return observation, reward, terminated, truncated, info
+
+    def reset(self, seed=None, options=None):
+        return observation, info
+
+    def render(self):
+
+    def close(self):
+        None
+
+
+if __name__ == "__main__":
+    env = CustomEnv(arg1, ...)
+    # It will check your custom environment and output additional warnings if needed
+    check_env(env)
--- PPO/test.py
+++ PPO/test.py
@@ -8,11 +8,11 @@ vec_env = make_vec_env("CartPole-v1", n_envs=4, seed=42)
 
 model = PPO("MlpPolicy", vec_env, verbose=1)
 model.learn(total_timesteps=250000)
-model.save("ppo_cartpole")
+model.save("./PPO/ppo_cartpole")
 
 del model # remove to demonstrate saving and loading
 
-model = PPO.load("ppo_cartpole")
+model = PPO.load("./PPO/ppo_cartpole")
 
 obs = vec_env.reset()
 while True:
