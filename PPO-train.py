38bbb98c50f26e21cb87b6f6e1e0905cbe675418
Elaphurus <105095085+LennoxLiu@users.noreply.github.com>
2023-08-27 08:55:08 +0200

    Update train code

--- PPO-train.py
+++ PPO-train.py
@@ -8,26 +8,23 @@ from tqdm import tqdm
 option={"argv": ["play","--no-gui","--agents","user_agent",\
                                             "coin_collector_agent", \
                                             "--scenario","loot-crate-4"]}
+model_path = "./Original/agent_code/PPO_agent/ppo_bomberman"
 env = CustomEnv(options = option)
 
 model = PPO("MlpPolicy", env, verbose=1)
-# model = PPO.load("./Original/agent_code/PPO_agent/ppo_bomberman", env)
+# model = PPO.load(model_path, env)
         
 for turn in tqdm(range(20000)):
-    if turn % 100 == 0: # reload environment for every 100 turns
+    if turn % 100 == 0 and turn != 0: # reload environment for every 100 turns
         del env
         env = CustomEnv(options = option)
         
+        model.save(model_path)
+        del model
+        model = PPO.load(model_path, env)
+
     model.learn(total_timesteps=400)
     if turn % 5 == 0:
-        model.save("./Original/agent_code/PPO_agent/ppo_bomberman")
+        model.save(model_path)
 
-model.save("./Original/agent_code/PPO_agent/ppo_bomberman")
-
-# del model # remove to demonstrate saving and loading
-# model = PPO.load("./agent_code/PPO_agent/_bomberman")
-
-# obs, _ = vec_env.reset()
-# for _ in range(400):
-#     action, _states = model.predict(obs)
-#     obs, rewards, terminated, truncated, info = vec_env.step(action)
+model.save(model_path)
