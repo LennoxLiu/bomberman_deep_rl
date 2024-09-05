03d50c9bc8bd4ba180ee2ca8902609db089c86e6
Lennox <105095085+LennoxLiu@users.noreply.github.com>
2024-09-05 22:24:26 +0200

    Update train_DAgger.py

--- train_DAgger.py
+++ train_DAgger.py
@@ -170,10 +170,10 @@ configs = {
         "l2_weight": 0, # 1e-7, default: 0
         "policy":{
             "learning_rate": 0.0003, # default 3e-4
-            "net_arch": [128, 64],
+            "net_arch": [256, 128, 64, 64, 64, 64, 32, 32],
             "features_extractor_class": "CustomCNN",
             "features_extractor_kwargs": {
-                "network_configs": {"cnn1":[128,128,128], "cnn2":[32,32], "features_dim": [128, 32]}
+                "network_configs": {"cnn1":[256,256,256], "cnn2":[64,64], "features_dim": [256, 64]}
         }}
     },
     "dagger_trainer": {
