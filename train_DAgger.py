139a3b841f6064237aaaf0751322b41b7220ee1b
Lennox <105095085+LennoxLiu@users.noreply.github.com>
2024-09-07 09:47:50 +0200

    Try smaller network

--- train_DAgger.py
+++ train_DAgger.py
@@ -67,7 +67,7 @@ configs = {
         "l2_weight": 1e-8, # 1e-7, default: 0
         "policy":{
             "learning_rate": 0.0003, # default 3e-4
-            "net_arch": dict(pi=[512,256,128,64,32], vf=[512, 512, 256, 256, 128, 128, 128, 128, 64, 64, 32, 32]), 
+            "net_arch": dict(pi=[256,128,64,32], vf=[256, 128, 64, 64, 64, 64 , 32, 32]), 
             "features_extractor_class": "CustomCNN",
             "activation_fn": "nn.ReLU", # nn.ReLU nn.LeakyReLU(slope), default: "th.nn.Tanh"
             "features_extractor_kwargs": {
